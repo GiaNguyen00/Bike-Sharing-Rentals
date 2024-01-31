cb65ba0f240ce94e2197485818e892fc472371c3
GiaNguyen <113059660+GiaNguyen00@users.noreply.github.com>
2024-01-31 12:16:08 -0500

    Project Finalization

---
+++ R codes/Reports/total_4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  8 12:24:30 2023
+
+@author: bach_nguyen
+"""
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+df = pd.read_csv('/Users/bach_nguyen/MA 575/Labs/Project/Data/day.csv')
+df.head()
